2fcc7bced01e45a869ce521160fdd891180efd89
samer <skababji@ehealthinformation.ca>
2022-09-10 21:59:21 -0400

    in develop: generated summary for most datasets

--- src/test.py
+++ src/test.py
@@ -1,8 +1,9 @@
+from operator import index
 from fuzzy_sql.fuzzy_sql import *
 import os  
 
 #set paths
-root_dir=Path(os.getcwd())
+root_dir=Path('/home/samer/projects/fuzzy_sql')
 real_dir=os.path.join(root_dir,'data/tabular/real')
 meta_dir=os.path.join(root_dir,'data/tabular/metadata')
 syn_dir=os.path.join(root_dir,'data/tabular/synthetic')
@@ -12,7 +13,9 @@ ds_names=extract_fnames(real_dir)
 # drop trail 5 since there is no synth data for it
 ds_names.remove('trial5')
 ds_names.remove('oncovid_dtd')
-ds_names.remove('danish')
+#ds_names.remove('danish') #this takes time
+ds_names.remove('trial3')
+ds_names.remove('trial6')
 
 real_path=[]
 meta_path=[]
@@ -23,8 +26,6 @@ for ds_name in ds_names:
     syn_path.append(syn_dir+f'/{ds_name}_syn_06.csv')
 
 
-
-
     # Fuzz
 res={'name':[], 'hlngr_mean':[], 'hlngr_median':[], 'hlngr_std_dev':[], 'ecldn_mean':[], 'ecldn_median':[], 'ecldn_std_dev':[] }
 queries=[]
@@ -33,14 +34,15 @@ for i,ds_name in enumerate(ds_names):
     #     continue
     scored_queries=fuzz_tabular(10,'twin_aggfltr', real_path[i], meta_path[i],syn_path[i])
     queries.append(scored_queries)
-    # res['name'].append(ds_name)
-    # hlngr_stats=calc_stats(scored_queries['hlngr_dist'])
-    # res['hlngr_mean'].append(hlngr_stats['mean'])
-    # res['hlngr_median'].append(hlngr_stats['median'])
-    # res['hlngr_std_dev'].append(hlngr_stats['stdev'])
-    # ecldn_stats=calc_stats(scored_queries['ecldn_dist'])
-    # res['ecldn_mean'].append(hlngr_stats['mean'])
-    # res['ecldn_median'].append(hlngr_stats['median'])
-    # res['ecldn_std_dev'].append(hlngr_stats['stdev'])
+    res['name'].append(ds_name)
+    hlngr_stats=calc_stats(scored_queries['hlngr_dist'])
+    res['hlngr_mean'].append(hlngr_stats['mean'])
+    res['hlngr_median'].append(hlngr_stats['median'])
+    res['hlngr_std_dev'].append(hlngr_stats['stddev'])
+    ecldn_stats=calc_stats(scored_queries['ecldn_dist'])
+    res['ecldn_mean'].append(hlngr_stats['mean'])
+    res['ecldn_median'].append(hlngr_stats['median'])
+    res['ecldn_std_dev'].append(hlngr_stats['stddev'])
 
-print('smk')
+res_df=pd.DataFrame(res)
+res_df.to_cs('result.csv', index=False)
