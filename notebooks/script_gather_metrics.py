80f3c4e7371915eb3055afae32ccdfb176196489
samer <skababji@ehealthinformation.ca>
2022-09-13 11:23:09 -0400

    eliminated csv write

--- notebooks/script_gather_metrics.py
+++ notebooks/script_gather_metrics.py
@@ -23,9 +23,10 @@ ds_names=extract_fnames(real_dir)
 # drop trail 5 since there is no synth data for it
 ds_names.remove('trial5')
 ds_names.remove('oncovid_dtd')
-#ds_names.remove('danish') #this takes time
 ds_names.remove('trial3')
 ds_names.remove('trial6')
+#ds_names.remove('danish') #takes time
+ds_names.remove('cchs') #takes time
 
 real_path=[]
 meta_path=[]
@@ -40,31 +41,22 @@ for ds_name in ds_names:
 res={'name':[], 'hlngr_mean':[], 'hlngr_median':[], 'hlngr_std_dev':[], 'ecldn_mean':[], 'ecldn_median':[], 'ecldn_std_dev':[] }
 queries=[]
 
-# with open('results.csv', 'a') as f:
-#     writer =writer(f)
-#     writer.writerow(list(res.keys()))
-with open('results.csv', 'a') as f:
-    writer =writer(f)
-    writer.writerow(list(res.keys()))
-    for i,ds_name in enumerate(ds_names):
-        # if ds_name !='trial2':
-        #     continue
-        scored_queries=fuzz_tabular(100,'twin_aggfltr', real_path[i], meta_path[i],syn_path[i])
-        queries.append(scored_queries)
-        res['name'].append(ds_name)
-        hlngr_stats=calc_stats(scored_queries['hlngr_dist'])
-        res['hlngr_mean'].append(hlngr_stats['mean'])
-        res['hlngr_median'].append(hlngr_stats['median'])
-        res['hlngr_std_dev'].append(hlngr_stats['stddev'])
-        ecldn_stats=calc_stats(scored_queries['ecldn_dist'])
-        res['ecldn_mean'].append(ecldn_stats['mean'])
-        res['ecldn_median'].append(ecldn_stats['median'])
-        res['ecldn_std_dev'].append(ecldn_stats['stddev'])
-        
-        row=[res[k][i] for k in res.keys()]
-        writer.writerow(row)
+for i,ds_name in enumerate(ds_names):
+    if ds_name !='danish':
+        continue
+    scored_queries=fuzz_tabular(1000,'twin_aggfltr', real_path[i], meta_path[i],syn_path[i])
+    queries.append(scored_queries)
+    res['name'].append(ds_name)
+    hlngr_stats=calc_stats(scored_queries['hlngr_dist'])
+    res['hlngr_mean'].append(hlngr_stats['mean'])
+    res['hlngr_median'].append(hlngr_stats['median'])
+    res['hlngr_std_dev'].append(hlngr_stats['stddev'])
+    ecldn_stats=calc_stats(scored_queries['ecldn_dist'])
+    res['ecldn_mean'].append(ecldn_stats['mean'])
+    res['ecldn_median'].append(ecldn_stats['median'])
+    res['ecldn_std_dev'].append(ecldn_stats['stddev'])
 
-#res_df=pd.DataFrame(res)
-#res_df.to_csv('result.csv', index=False)
+res_df=pd.DataFrame(res)
+res_df.to_csv('results.csv', index=False)
 
 
